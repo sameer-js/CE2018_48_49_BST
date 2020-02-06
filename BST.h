f8ef57db9455b9ec3f604579e6e65911579b89e1
Kucat <name =>
2020-02-05 20:03:44 -0800

    added a function max

--- BST.h
+++ BST.h
@@ -6,7 +6,7 @@ class BST{
 		virtual void add(int data)=0;
 		virtual void preOrderTraversal(int index)=0;
 		virtual bool search(int data)=0;
-		virtual int min()=0;
+		virtual int max()=0;
 };
 
 #endif
